667863ca8882e3a4ad55cbd9530b681415b3ab75
John Coleman <jbcolemanjr@gmail.com>
2023-10-10 12:58:33 -0400

    coffee shop complete

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,34 +1,73 @@
+from statistics import mean
+
 class Coffee:
+    all=[]
+
     def __init__(self, name):
         self.name = name
+        Coffee.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if type(name) == str and len(name) >=3 and not hasattr(self, "name"):
+            self._name = name
         
     def orders(self):
-        pass
+        return [order for order in Order.all if order.coffee == self]
     
     def customers(self):
-        pass
+        return list({order.customer for order in self.orders()})
     
     def num_orders(self):
-        pass
+        return len([order for order in self.orders()])
     
     def average_price(self):
-        pass
+        prices = [order.price for order in self.orders()]
+        return mean(prices)
 
 class Customer:
+    all=[]
+
     def __init__(self, name):
         self.name = name
+        Customer.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if type(name) == str and 1 <= len(name) <= 15:
+            self._name = name
         
     def orders(self):
-        pass
+        return [order for order in Order.all if order.customer == self]
     
     def coffees(self):
-        pass
+        return list({order.coffee for order in self.orders()})
     
     def create_order(self, coffee, price):
-        pass
+        return Order(self, coffee, price)
     
 class Order:
+    all=[]
+
     def __init__(self, customer, coffee, price):
         self.customer = customer
         self.coffee = coffee
         self.price = price
+        Order.all.append(self)
+
+    @property
+    def price(self):
+        return self._price
+    
+    @price.setter
+    def price(self, price):
+        if type(price) == float and 1.0 <= price <= 10.0 and not hasattr(self, "price"):
+            self._price = price
